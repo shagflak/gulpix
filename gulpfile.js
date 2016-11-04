b1aa3cbddccc1d81699e2f897b1f6811c63eef6d
Figueroa Juan <c_juanca@qualcomm.com>
2016-11-04 16:48:18 -0600

    Added more structure to the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var webserver  = require('gulp-webserver');
 var lazypipe   = require("lazypipe");
 var babel      = require('gulp-babel');
 
-//Paths to files
 var app = {
     paths: {
         scripts : 'app/scripts/**/*.js',
@@ -16,19 +15,28 @@ var app = {
     }
 };
 
+var transpileClient =
+        gulp.src('app/**/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('.tmp'));
 
-gulp.task('build', function() { return ''; });
 
+
+//Tasks::Prod
+gulp.task('build', function() { return compileScripts(); });
+
+//Tasks:dev
+//lazy pipes are used when you want to set an order of execution in your pipes if you don't add the lazypipe for the
+//transpileClient function it will not execute on the right order (haven't digged depper on this)
+//
 gulp.task('transpile:client', function() {
-    return  gulp.src(app.paths.scripts)
-            .pipe(babel({
-                presets: ['es2015','react']
-            }))
-            .pipe(gulp.dest('.tmp'));
+    return  transpileClient();
 });
 
 gulp.task('dev:copy-index', function(){
-    ´gulp.src(app.paths.index)
+    gulp.src(app.paths.index)
     .pipe(gulp.dest('.tmp'));
 });
 
@@ -54,9 +62,12 @@ gulp.task('dev:watch', function() {
     gulp.watch(app.paths.index, ['dev:copy-index']);
 });
 
-//Main tasks
 gulp.task('default', ['dev:copy-index','dev:watch','webserver']);
 //gulp.task('prod:dist',[]); //under development
+//
+
+
+
 
 /**
  *  Notes:
